b4140c028f24b92f5206c2e0ef60d3714c5ffd7d
Jack Rathwell <jrathwell@lcs.on.ca>
2025-03-03 14:29:39 -0500

    Added a very basic view to show the current joke.

--- JokeFinder/ViewModel/JokeViewModel.swift
+++ JokeFinder/ViewModel/JokeViewModel.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+
+import Foundation
+ 
+@Observable
+class JokeViewModel {
+    
+    // MARK: Stored properties
+    
+    // Whatever joke has most recently been downloaded
+    // from the endpoint
+    var currentJoke: Joke?
+    
+    // MARK: Initializer(s)
+    init(currentJoke: Joke? = nil) {
+        
+        // Take whatever joke was provided when an instance of
+        // this view model is created, and make it the current joke.
+        //
+        // Otherwise, the default value for the current joke
+        // will be a nil.
+        self.currentJoke = currentJoke
+    }
+    
+}
