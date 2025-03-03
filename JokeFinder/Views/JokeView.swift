4b174dabea2e27d2b6c5cfea366c116e5525dc04
Jack Rathwell <jrathwell@lcs.on.ca>
2025-03-03 14:23:22 -0500

    Initial project organization; added groups for MVVM design pattern.

---
+++ JokeFinder/Views/JokeView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  JokeFinder
+//
+//  Created by Jack William Rathwell on 2025-03-03.
+//
+
+import SwiftUI
+
+struct JokeView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    JokeView()
+}
